22ac682d0a566df4250e6bd78f4f2bd00beedcc4
Jim Jeffery <jim.jeffery@gmail.com>
2019-12-10 13:26:16 +0000

    Initial Commit

---
+++ CardParts/CardParts.h
@@ -0,0 +1,19 @@
+//
+//  CardParts.h
+//  CardParts
+//
+//  Created by James Jeffery on 10/12/2019.
+//  Copyright © 2019 FCSL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardParts.
+FOUNDATION_EXPORT double CardPartsVersionNumber;
+
+//! Project version string for CardParts.
+FOUNDATION_EXPORT const unsigned char CardPartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardParts/PublicHeader.h>
+
+
